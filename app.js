3120441d30f7535ec7cb289208be1e40c574e5af
0ho0ho <y2k6273@naver.com>
2022-06-06 15:58:10 +0900

    promise

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
-const http = require("http");
+const { readFile } = require("fs");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Home Page");
-    return;
-  }
-  if (req.url === "/about") {
-    // Blocking code
-    for (let i = 0; i < 1000; i++) {
-      for (let j = 0; j < 1000; j++) {
-        console.log(`${i} ${j}`);
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
       }
-    }
-    res.end("About Page");
-    return;
-  }
-  res.end("Error Page");
-});
+    });
+  });
+};
 
-server.listen(5000, () => {
-  console.log("Server Listening on port 5000...");
-});
+getText("./content/first.txt")
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
