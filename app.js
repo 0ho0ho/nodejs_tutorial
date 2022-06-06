b22885eb8f25511a4fa2ef175fe43e09d870b25a
0ho0ho <y2k6273@naver.com>
2022-06-06 15:42:28 +0900

    blocking code

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const _ = require("lodash");
+const http = require("http");
 
-const items = [1, [2, [3, [4]]]];
-const newItems = _.flattenDeep(items);
-console.log(newItems);
-console.log("hello people!");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Home Page");
+    return;
+  }
+  if (req.url === "/about") {
+    // Blocking code
+    for (let i = 0; i < 1000; i++) {
+      for (let j = 0; j < 1000; j++) {
+        console.log(`${i} ${j}`);
+      }
+    }
+    res.end("About Page");
+    return;
+  }
+  res.end("Error Page");
+});
+
+server.listen(5000, () => {
+  console.log("Server Listening on port 5000...");
+});
